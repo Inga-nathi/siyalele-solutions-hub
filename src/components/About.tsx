4643cfa80e486c382f4e6a4fa12f5f7e68728b33
Inga-nathi <inganathimkabile85@gmail.com>
2026-02-24 00:55:28 +0200

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,13 +13,13 @@ const About = () => {
   const certifications = [
     { label: "Company Registration", value: "2023/000257/07" },
     { label: "CSD Verification", value: "MAAA1487648" },
+    { label: "COIDA Reg Number", value: "990001513880" },
     { label: "Eskom Vendor", value: "11094569" },
+    { label: "Afrimat Vendor Number", value: "SIY-013" },
     { label: "Agricultural Research Council Supplier Number", value: "SUPL008169" },
     { label: "Tax Clearance", value: "9279622253" },
     { label: "VAT Number", value: "4410317988" },
     { label: "Banking", value: "ABSA Business Account" },
-    { label: "Afrimat Vendor Number", value: "SIY-013" },
-    { label: "COIDA Reg Number", value: "990001513880" },
   ];
 
   return (
